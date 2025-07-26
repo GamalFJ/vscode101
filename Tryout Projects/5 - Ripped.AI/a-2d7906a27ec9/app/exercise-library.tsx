8669406370286375c4a9bf1edb79c6eae6a63a91
Gamal Jastram <gamalfjastram@gmail.com>
2025-07-26 19:42:23 -0400

    Ripped.AI delete

--- Tryout Projects/5 - Ripped.AI/a-2d7906a27ec9/app/exercise-library.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ExerciseLibrary from '../components/ExerciseLibrary';
-
-export default function ExerciseLibraryScreen() {
-  return <ExerciseLibrary />;
-}
--- Tryout Projects/5 - Ripped.AI/a-2d7906a27ec9/utils/theme.ts
+++
@@ -1,2 +0,0 @@
-// Re-export everything from theme.tsx
-export * from './theme.tsx';
